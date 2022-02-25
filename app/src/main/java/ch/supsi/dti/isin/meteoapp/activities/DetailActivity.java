000a932e4addbdc8ddb8cacb58e901d8d4e22d6b
matteo.metaldi <matteo.metaldi@student.supsi.ch>
2022-02-25 15:25:57 +0100

    added new version

--- app/src/main/java/ch/supsi/dti/isin/meteoapp/activities/DetailActivity.java
+++ app/src/main/java/ch/supsi/dti/isin/meteoapp/activities/DetailActivity.java
@@ -2,13 +2,18 @@ package ch.supsi.dti.isin.meteoapp.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import java.util.UUID;
 
+import ch.supsi.dti.isin.meteoapp.R;
 import ch.supsi.dti.isin.meteoapp.fragments.DetailLocationFragment;
 
-public class DetailActivity extends SingleFragmentActivity {
+public class DetailActivity extends AppCompatActivity {
     private static final String EXTRA_LOCATION_ID = "ch.supsi.dti.isin.meteoapp.location_id";
 
     public static Intent newIntent(Context packageContext, UUID locationId) {
@@ -18,8 +23,17 @@ public class DetailActivity extends SingleFragmentActivity {
     }
 
     @Override
-    protected Fragment createFragment() {
-        UUID locationId = (UUID) getIntent().getSerializableExtra(EXTRA_LOCATION_ID);
-        return new DetailLocationFragment().newInstance(locationId);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_single_fragment);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            UUID locationId = (UUID) getIntent().getSerializableExtra(EXTRA_LOCATION_ID);
+            fragment = new DetailLocationFragment().newInstance(locationId);
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/ch/supsi/dti/isin/meteoapp/activities/MainActivity.java
+++ app/src/main/java/ch/supsi/dti/isin/meteoapp/activities/MainActivity.java
@@ -1,13 +1,26 @@
 package ch.supsi.dti.isin.meteoapp.activities;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import ch.supsi.dti.isin.meteoapp.R;
 import ch.supsi.dti.isin.meteoapp.fragments.ListFragment;
 
-public class MainActivity extends SingleFragmentActivity {
-
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected Fragment createFragment() {
-        return new ListFragment();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_single_fragment);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new ListFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
